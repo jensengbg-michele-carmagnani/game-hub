7aec8e926cc66fb93ab4280ba3466e8ba0c0f805
Karma <michele.carmagnani@gmail.com>
2023-09-22 17:15:01 +0200

    feat: add npm ms for calcolation time

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-
+import ms from "ms";
 import APIClient from "../services/api-client";
 
 export type Genre = {
@@ -15,7 +15,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("1d"),
     initialData: genres,
   });
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,3 +1,4 @@
+import ms from "ms";
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 
@@ -14,7 +15,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["/platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("1d"),
     initialData: genres,
   });
 
