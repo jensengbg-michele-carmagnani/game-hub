103e611da623bea336e103c32a834dc4ced1ac59
Karma <michele.carmagnani@gmail.com>
2023-09-22 17:24:56 +0200

    refacto calculation time

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import ms from "ms";
 import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatforms";
@@ -29,7 +30,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("24h"), // 24 hours
   });
 
 export default useGames;
